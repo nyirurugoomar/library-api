e050958b2d5255d8aa443a888dd4f9e618275f69
Omar <nyirurugoomar@gmail.com>
2024-11-25 17:45:27 +0200

    Authentication, Login/Sign Up, assign JWT

--- dist/app.module.js
+++ dist/app.module.js
@@ -13,6 +13,7 @@ const app_service_1 = require("./app.service");
 const book_module_1 = require("./book/book.module");
 const config_1 = require("@nestjs/config");
 const mongoose_1 = require("@nestjs/mongoose");
+const auth_module_1 = require("./auth/auth.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -24,7 +25,8 @@ exports.AppModule = AppModule = __decorate([
                 isGlobal: true,
             }),
             mongoose_1.MongooseModule.forRoot(process.env.DB_URI),
-            book_module_1.BookModule
+            book_module_1.BookModule,
+            auth_module_1.AuthModule
         ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
---
+++ dist/auth/schemas/user.schema.d.ts
@@ -0,0 +1,14 @@
+export declare class User {
+    name: string;
+    email: string;
+    password: string;
+}
+export declare const UserSchema: import("mongoose").Schema<User, import("mongoose").Model<User, any, any, any, import("mongoose").Document<unknown, any, User> & User & {
+    _id: import("mongoose").Types.ObjectId;
+} & {
+    __v: number;
+}, any>, {}, {}, {}, {}, import("mongoose").DefaultSchemaOptions, User, import("mongoose").Document<unknown, {}, import("mongoose").FlatRecord<User>> & import("mongoose").FlatRecord<User> & {
+    _id: import("mongoose").Types.ObjectId;
+} & {
+    __v: number;
+}>;
