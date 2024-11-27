53c62534970417aea4aa39144819484e27c0414f
Omar <nyirurugoomar@gmail.com>
2024-11-27 09:35:10 +0200

    Update api response

--- dist/book/book.controller.js
+++ dist/book/book.controller.js
@@ -54,7 +54,7 @@ __decorate([
     (0, swagger_1.ApiOperation)({ summary: 'Fetch a list of books' }),
     (0, swagger_1.ApiResponse)({
         status: 200,
-        description: "List of book fetched successfully"
+        description: 'List of book fetched successfully',
     }),
     (0, swagger_1.ApiNotFoundResponse)({ description: 'Fail to fetch list of books' }),
     (0, swagger_1.ApiOkResponse)({
@@ -74,12 +74,12 @@ __decorate([
     (0, common_1.Post)(),
     (0, swagger_1.ApiOperation)({ summary: 'Create new book' }),
     (0, swagger_1.ApiCreatedResponse)({
-        description: "Book created successfully"
+        description: 'Book created successfully',
     }),
     (0, swagger_1.ApiOkResponse)({
         description: 'Book created successfully',
         type: create_book_dto_1.CreateBookDto,
-        isArray: true
+        isArray: true,
     }),
     (0, swagger_1.ApiBadRequestResponse)({
         description: 'Invalid data provided',
@@ -94,7 +94,7 @@ __decorate([
 __decorate([
     (0, common_1.Get)(':id'),
     (0, swagger_1.ApiOperation)({ summary: 'Fetch book by id' }),
-    (0, swagger_1.ApiResponse)({ description: "Book found" }),
+    (0, swagger_1.ApiResponse)({ status: 200, description: 'Book found' }),
     (0, swagger_1.ApiNotFoundResponse)({ description: 'Book not found.' }),
     __param(0, (0, common_1.Param)('id')),
     __metadata("design:type", Function),
@@ -103,6 +103,13 @@ __decorate([
 ], BookController.prototype, "getBook", null);
 __decorate([
     (0, common_1.Put)(':id'),
+    (0, swagger_1.ApiOperation)({ summary: 'Update book' }),
+    (0, swagger_1.ApiOkResponse)({ description: 'Book updated successfully' }),
+    (0, swagger_1.ApiNotFoundResponse)({
+        description: 'Book not found',
+        type: update_book_dto_1.UpdateBookDto,
+        isArray: true
+    }),
     __param(0, (0, common_1.Param)('id')),
     __param(1, (0, common_1.Body)()),
     __metadata("design:type", Function),
@@ -111,6 +118,9 @@ __decorate([
 ], BookController.prototype, "updateBook", null);
 __decorate([
     (0, common_1.Delete)(':id'),
+    (0, swagger_1.ApiOperation)({ summary: 'Delete book by id' }),
+    (0, swagger_1.ApiResponse)({ status: 200, description: 'Book deleted successfully' }),
+    (0, swagger_1.ApiNotFoundResponse)({ description: 'Id of book not found' }),
     __param(0, (0, common_1.Param)('id')),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [String]),
@@ -118,18 +128,21 @@ __decorate([
 ], BookController.prototype, "deleteBook", null);
 __decorate([
     (0, common_1.Put)('upload/:id'),
+    (0, swagger_1.ApiOperation)({ summary: 'Upload image' }),
+    (0, swagger_1.ApiResponse)({ status: 200, description: 'Upload image successfully' }),
     (0, common_1.UseGuards)((0, passport_1.AuthGuard)()),
     (0, common_1.UseInterceptors)((0, platform_express_1.FilesInterceptor)('files')),
     __param(0, (0, common_1.Param)('id')),
-    __param(1, (0, common_1.UploadedFiles)(new common_1.ParseFilePipeBuilder().addFileTypeValidator({
+    __param(1, (0, common_1.UploadedFiles)(new common_1.ParseFilePipeBuilder()
+        .addFileTypeValidator({
         fileType: /(jpg|png|jpeg)$/,
     })
         .addMaxSizeValidator({
         maxSize: 1000 * 1000,
-        message: 'File must be less than 10Mb'
+        message: 'File must be less than 10Mb',
     })
         .build({
-        errorHttpStatusCode: common_1.HttpStatus.UNPROCESSABLE_ENTITY
+        errorHttpStatusCode: common_1.HttpStatus.UNPROCESSABLE_ENTITY,
     }))),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [String, Array]),
--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,119 +1,149 @@
-import { Body, Controller, Get,Param,Post,Put,Delete, Query, UseGuards, Req, UseInterceptors, UploadedFiles, ParseFilePipeBuilder, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Query,
+  UseGuards,
+  Req,
+  UseInterceptors,
+  UploadedFiles,
+  ParseFilePipeBuilder,
+  HttpStatus,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 
-import {Query as ExpressQuery} from 'express-serve-static-core'
+import { Query as ExpressQuery } from 'express-serve-static-core';
 import { AuthGuard } from '@nestjs/passport';
 import { Roles } from '../auth/decorators/role.decorator';
 import { Role } from '../auth/enums/role.enum';
 import { RoleGuard } from '../auth/guards/roles.guard';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { SkipThrottle } from '@nestjs/throttler';
-import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { BookDto } from './dto/book.dto';
-
 
 @ApiBearerAuth()
 @Controller('books')
 export class BookController {
-    constructor(private bookService: BookService){}
-    @Get()
-    @ApiOperation({summary:'Fetch a list of books'})
-    @ApiResponse({
-        status:200,
-        description:"List of book fetched successfully"
-    })
-    @ApiNotFoundResponse({description:'Fail to fetch list of books'})
-    @ApiOkResponse({
-        description:'List of books retrived successfully',
-        type:BookDto,
-        isArray: true,    
-    })
-    @SkipThrottle()
-    @UseGuards(AuthGuard(),RoleGuard)
-    @Roles(Role.Moderator, Role.User)
-    async getAllBooks(
-        @Query()
-        query : ExpressQuery): Promise<Book[]>{
-        return this.bookService.findAll(query);
-    }
-    @Post()
-    @ApiOperation({summary:'Create new book'})
-    @ApiCreatedResponse({
-        description:"Book created successfully"
-    })
-    @ApiOkResponse({
-        description:'Book created successfully',
-        type:CreateBookDto,
-        isArray: true
-    })
-    @ApiBadRequestResponse({
-        description:'Invalid data provided',
-        
-    })
+  constructor(private bookService: BookService) {}
+  @Get()
+  @ApiOperation({ summary: 'Fetch a list of books' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of book fetched successfully',
+  })
+  @ApiNotFoundResponse({ description: 'Fail to fetch list of books' })
+  @ApiOkResponse({
+    description: 'List of books retrived successfully',
+    type: BookDto,
+    isArray: true,
+  })
+  @SkipThrottle()
+  @UseGuards(AuthGuard(), RoleGuard)
+  @Roles(Role.Moderator, Role.User)
+  async getAllBooks(
+    @Query()
+    query: ExpressQuery,
+  ): Promise<Book[]> {
+    return this.bookService.findAll(query);
+  }
+  @Post()
+  @ApiOperation({ summary: 'Create new book' })
+  @ApiCreatedResponse({
+    description: 'Book created successfully',
+  })
+  @ApiOkResponse({
+    description: 'Book created successfully',
+    type: CreateBookDto,
+    isArray: true,
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid data provided',
+  })
+  @UseGuards(AuthGuard())
+  async createBook(
+    @Body()
+    book: CreateBookDto,
+    @Req() req,
+  ): Promise<Book> {
+    return this.bookService.create(book, req.user);
+  }
 
-    @UseGuards(AuthGuard())
-    async createBook(
-        @Body()
-        book:CreateBookDto,
-        @Req() req
-        ):Promise<Book>{
-            
-        return this.bookService.create(book,req.user)
-    }
+  @Get(':id')
+  @ApiOperation({ summary: 'Fetch book by id' })
+  @ApiResponse({ status:200,description: 'Book found' })
+  @ApiNotFoundResponse({ description: 'Book not found.' })
+  async getBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.findById(id);
+  }
 
-    @Get(':id')
-    @ApiOperation({summary:'Fetch book by id'})
-    @ApiResponse({description:"Book found"})
-    @ApiNotFoundResponse({description:'Book not found.'})
-    async getBook(
-        @Param('id')
-        id:string
-    ): Promise<Book>{
-        return this.bookService.findById(id);
-    }
+  @Put(':id')
+  @ApiOperation({summary: 'Update book'})
+  @ApiOkResponse({description:'Book updated successfully'})
+  @ApiNotFoundResponse({ 
+    description: 'Book not found',
+    type:UpdateBookDto,
+    isArray:true
+ })
+  async updateBook(
+    @Param('id')
+    id: string,
+    @Body() book: UpdateBookDto,
+  ): Promise<Book> {
+    return this.bookService.updateById(id, book);
+  }
 
+  @Delete(':id')
+  @ApiOperation({summary:'Delete book by id'})
+  @ApiResponse({status:200,description: 'Book deleted successfully'})
+  @ApiNotFoundResponse({description:'Id of book not found'})
+  async deleteBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.deleteById(id);
+  }
 
-    @Put(':id')
-    async updateBook(
-        @Param('id')
-        id:string,
-        @Body()book:UpdateBookDto,):Promise<Book>{
-        return this.bookService.updateById(id, book);
-    }
-
-    @Delete(':id')
-    async deleteBook(
-        @Param('id')
-        id:string
-    ): Promise<Book>{
-        return this.bookService.deleteById(id);
-    }
-
-
-
-    @Put('upload/:id')
-    @UseGuards(AuthGuard())
-    @UseInterceptors(FilesInterceptor('files'))
-    async uploadImages(
-     @Param('id') id:string,
-     @UploadedFiles(
-        new ParseFilePipeBuilder().addFileTypeValidator({
-            fileType: /(jpg|png|jpeg)$/,
+  @Put('upload/:id')
+  @ApiOperation({summary:'Upload image'})
+  @ApiResponse({status:200,description: 'Upload image successfully'})
+  @UseGuards(AuthGuard())
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadImages(
+    @Param('id') id: string,
+    @UploadedFiles(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /(jpg|png|jpeg)$/,
         })
         .addMaxSizeValidator({
-            maxSize:1000 * 1000,
-            message:'File must be less than 10Mb'
+          maxSize: 1000 * 1000,
+          message: 'File must be less than 10Mb',
         })
         .build({
-            errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
-        })
-     ) files: Array <Express.Multer.File>
-
-    ){
-        return this.bookService.uploadImages(id,files)
-    }
-
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    files: Array<Express.Multer.File>,
+  ) {
+    return this.bookService.uploadImages(id, files);
+  }
 }
